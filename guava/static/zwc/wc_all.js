22188e3a3873061b0b8691dc381147b0ce4886f9
pubs <pubudu79@gmail.com>
2023-10-29 00:09:57 +0000

    Add existing project files to Git

---
+++ guava/static/zwc/wc_all.js
@@ -0,0 +1,15 @@
+import WCInput from  '/webui/guava/guava/static/zwc/wc_input.js' ;
+import WCHidden from  '/webui/guava/guava/static/zwc/wc_hidden.js' ;
+import WCTab from  '/webui/guava/guava/static/zwc/wc_tab.js' ;
+
+import WCTable from  '/webui/guava/guava/static/zwc/wc_table.js' ;
+import WCCheckbox from  '/webui/guava/guava/static/zwc/wc_checkbox.js' ;
+import WCModal from  '/webui/guava/guava/static/zwc/wc_modal.js' ;
+import WCRadio from  '/webui/guava/guava/static/zwc/wc_radio.js' ;
+import WCNav from  '/webui/guava/guava/static/zwc/wc_nav.js' ;
+
+import WCMessage from  '/webui/guava/guava/static/zwc/wc_message.js' ;
+
+import WCGroup from  '/webui/guava/guava/static/zwc/wc_group.js' ;
+import WCForm from  '/webui/guava/guava/static/zwc/wc_form.js' ;
+
